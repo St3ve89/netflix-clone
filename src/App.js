fe2c41c10fa3647a019075d775f2886d82ad5906
St3ve89 <istvan.acs89@gmail.com>
2021-02-09 18:04:24 +0000

    Start setting up react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <JumbotronContainer />
+        <FaqsContainer />
+        <FooterContainer />
+      </Route>
+    </Router>
   );
 }
